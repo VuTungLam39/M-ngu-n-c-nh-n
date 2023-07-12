137539fb4e0de8ad103a7b395e4ff79b1718fb9e
VuTungLam39 <125626232+VuTungLam39@users.noreply.github.com>
2023-07-12 22:22:47 +0700

    Add files via upload

---
+++ tính tổng các phần tử của 1 mảng.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class TongCacPhanTuTrongMang {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Nhập số lượng phần tử của mảng: ");
+        int n = scanner.nextInt();
+
+        int[] arr = new int[n];
+        System.out.println("Nhập các phần tử của mảng:");
+        for (int i = 0; i < n; i++) {
+            System.out.print("Phần tử thứ " + (i + 1) + ": ");
+            arr[i] = scanner.nextInt();
+        }
+
+        int sum = 0;
+        for (int i = 0; i < n; i++) {
+            sum += arr[i];
+        }
+
+        System.out.println("Tổng các phần tử trong mảng: " + sum);
+
+        scanner.close();
+    }
+}
